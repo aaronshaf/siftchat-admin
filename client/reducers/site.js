5af45f5e45c137f4f4255c661c87f8c41b63ae1e
Aaron Shafovaloff <aaronshaf@gmail.com>
2016-03-26 18:00:43 -0600

    improvements to site reducer

--- client/reducers/site.js
+++ client/reducers/site.js
@@ -1,20 +1,44 @@
 import {
-  // GET_SITE_PENDING
-  GET_SITE_SUCCESS
+  GET_SITE_PENDING,
+  GET_SITE_SUCCESS,
+  GET_SITE_ERROR
 } from '../constants/action-types'
 
 const initialState = {
-  data: {}
+  data: {},
+  wasSuccessful: true,
+  isLoading: false,
+  error: null
 }
 
 export default function article (state = initialState, action) {
   switch (action.type) {
-    case GET_SITE_SUCCESS:
-      const newState = {
+    case GET_SITE_PENDING:
+      return {
         ...state,
-        data: action.response.data
+        data: {},
+        wasSuccessful: false,
+        isLoading: true,
+        error: null
       }
-      return newState
+
+    case GET_SITE_SUCCESS:
+      return {
+        ...state,
+        data: action.response.data,
+        wasSuccessful: true,
+        isLoading: false,
+        error: null
+      }
+
+    case GET_SITE_ERROR:
+      return {
+        ...state,
+        data: {},
+        wasSuccessful: false,
+        isLoading: false,
+        error: action.error
+      }
     default:
       return state
   }
